b8bd8656eab17ede67c6f02c9154413c93efd111
Caspec <quii2000@yahoo.dk>
2020-09-14 10:27:22 +0200

    -

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import VueResource from 'vue-resource'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
